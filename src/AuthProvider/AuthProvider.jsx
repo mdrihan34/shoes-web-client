c27c180825ced403aefbc1e912dbab714aa0917f
mdrihan34 <mdrihan0019@gmail.com>
2025-04-08 18:11:02 +0600

    fix Firebase path

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -6,7 +6,7 @@ import {
   signOut,
 } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
-import app from "../Firbase/firebase-config"; 
+import app from "../Firebase/firebase-config"; 
 
 
 
